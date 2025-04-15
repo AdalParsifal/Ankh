a20ed2c38c97863285c43b16519451bcd1623a0a
AdalParsifal <albertobustac@gmail.com>
2025-04-15 19:25:11 -0400

    ghost views and urls to run

--- webankh/urls.py
+++ webankh/urls.py
@@ -3,5 +3,13 @@ from . import views
 
 urlpatterns = [
     path('bienvenida/', views.bienvenida, name='bienvenida'),
-    path('', views.inicio, name='inicio'),  # Asumimos que esta ya existe
+    
+    path('', views.inicio, name='inicio'),  
+
+    path('nosotros/', views.nosotros, name='nosotros'),
+    path('publicaciones/', views.publicaciones, name='publicaciones'),
+    path('faq/', views.faq, name='faq'),
+    path('glnchile/', views.glnchile, name='glnchile'),
+    path('biblioteca/', views.biblioteca, name='biblioteca'),
+    path('noticias/', views.noticias, name='noticias'),
 ]
--- webankh/views.py
+++ webankh/views.py
@@ -14,3 +14,21 @@ def bienvenida(request):
 
 def inicio(request):
     return render(request, 'inicio.html')
+
+def nosotros(request):
+    return render(request, 'nosotros.html')
+
+def publicaciones(request):
+    return render(request, 'publicaciones.html')
+
+def faq(request):
+    return render(request, 'faq.html')
+
+def glnchile(request):
+    return render(request, 'glnchile.html')
+
+def biblioteca(request):
+    return render(request, 'biblioteca.html')
+
+def noticias(request):
+    return render(request, 'noticias.html')
