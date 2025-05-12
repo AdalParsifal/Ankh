ee2407d93d6bb560b67979034dc494a8697fcb57
AdalParsifal <albertobustac@gmail.com>
2025-05-12 13:30:58 -0400

    admin de publicaciones

--- webankh/admin.py
+++ webankh/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Publicacion
 
-# Register your models here.
+@admin.register(Publicacion)
+class PublicacionAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'fecha_publicacion')
+    search_fields = ('titulo',)
+    list_filter = ('fecha_publicacion',)
+    ordering = ('-fecha_publicacion',)
+
+
